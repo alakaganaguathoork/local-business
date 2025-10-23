ed4ce8bf3d3e800f0ca139aae73779a2b740678e
Misha P <alakaganaguathoork@gmail.com>
2025-10-24 00:11:48 +0300

    Add /helm

--- terraform/kubernetes/locals.tf
+++ terraform/kubernetes/locals.tf
@@ -5,21 +5,21 @@ locals {
 
   helm_releases = {
     argocd = {
-      values_file_url = "https://raw.githubusercontent.com/alakaganaguathoork/local-business-open-api-project/refs/heads/main/kubernetes/helm/helpers/argocd/argocd-custom-values-aws.yaml"
+      values_file_url = "https://raw.githubusercontent.com/alakaganaguathoork/local-business/refs/heads/main/helm/helpers/argocd/argocd-custom-values-aws.yaml"
       name            = "argocd"
       namespace       = "argocd"
       repository      = "https://argoproj.github.io/argo-helm"
       chart           = "argo-cd"
     }
     prometheus = {
-      values_file_url = "https://raw.githubusercontent.com/alakaganaguathoork/local-business-open-api-project/refs/heads/main/kubernetes/helm/helpers/prometheus/prometheus-custom-values-aws.yaml"
+      values_file_url = "https://raw.githubusercontent.com/alakaganaguathoork/local-business/refs/heads/main/helm/helpers/prometheus/prometheus-custom-values-aws.yaml"
       name            = "prometheus"
       namespace       = "monitoring"
       repository      = "https://prometheus-community.github.io/helm-charts"
       chart           = "prometheus"
     }
     grafana = {
-      values_file_url = "https://raw.githubusercontent.com/alakaganaguathoork/local-business-open-api-project/refs/heads/main/kubernetes/helm/helpers/grafana/grafana-custom-values-aws.yaml"
+      values_file_url = "https://raw.githubusercontent.com/alakaganaguathoork/local-business/refs/heads/main/helm/helpers/grafana/grafana-custom-values-aws.yaml"
       name            = "grafana"
       namespace       = "monitoring"
       repository      = "https://grafana.github.io/helm-charts"
