b1e4f287244f9fee3609b38ca0e48a0761797f53
Misha P <alakaganaguathoork@gmail.com>
2025-10-24 03:13:42 +0300

    Add  to helm_release terraform module

--- terraform/kubernetes/locals.tf
+++ terraform/kubernetes/locals.tf
@@ -11,13 +11,13 @@ locals {
       repository      = "https://argoproj.github.io/argo-helm"
       chart           = "argo-cd"
     }
-    prometheus = {
-      values_file_url = "https://raw.githubusercontent.com/alakaganaguathoork/local-business/refs/heads/main/helm/helpers/prometheus/prom-custom-values-aws.yaml"
-      name            = "prometheus"
-      namespace       = "monitoring"
-      repository      = "https://prometheus-community.github.io/helm-charts"
-      chart           = "prometheus"
-    }
+    # prometheus = {
+      # values_file_url = "https://raw.githubusercontent.com/alakaganaguathoork/local-business/refs/heads/main/helm/helpers/prometheus/prom-custom-values-aws.yaml"
+      # name            = "prometheus"
+      # namespace       = "monitoring"
+      # repository      = "https://prometheus-community.github.io/helm-charts"
+      # chart           = "prometheus"
+    # }
     grafana = {
       values_file_url = "https://raw.githubusercontent.com/alakaganaguathoork/local-business/refs/heads/main/helm/helpers/grafana/grafana-custom-values-aws.yaml"
       name            = "grafana"
