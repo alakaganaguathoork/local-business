3a6298a24f0fff73faa2b1ec0892fcac2112491f
Misha P <alakaganaguathoork@gmail.com>
2025-10-28 03:30:39 +0200

    Add Alloy

--- terraform/kubernetes/locals.tf
+++ terraform/kubernetes/locals.tf
@@ -5,14 +5,14 @@ locals {
 
   helm_releases = {
     argocd = {
-      values_file_url = "https://raw.githubusercontent.com/alakaganaguathoork/local-business/refs/heads/main/helm/helpers/argocd/argocd-custom-values-aws.yaml"
+      values_file_url = "https://raw.githubusercontent.com/alakaganaguathoork/local-business/refs/heads/main/terraform/kubernetes/custom-values/argocd/argocd-custom-values-aws.yaml"
       name            = "argocd"
       namespace       = "argocd"
       repository      = "https://argoproj.github.io/argo-helm"
       chart           = "argo-cd"
     }
     prometheus = {
-      values_file_url = "https://raw.githubusercontent.com/alakaganaguathoork/local-business/refs/heads/main/helm/helpers/prometheus/prom-custom-values-aws.yaml"
+      values_file_url = "https://raw.githubusercontent.com/alakaganaguathoork/local-business/refs/heads/main/terraform/kubernetes/custom-values/prometheus/prom-custom-values-aws.yaml"
       name            = "prometheus"
       namespace       = "monitoring"
       repository      = "https://prometheus-community.github.io/helm-charts"
@@ -20,21 +20,21 @@ locals {
       create_ingress  = true
     }
     grafana = {
-      values_file_url = "https://raw.githubusercontent.com/alakaganaguathoork/local-business/refs/heads/main/helm/helpers/grafana/grafana-custom-values-aws.yaml"
+      values_file_url = "https://raw.githubusercontent.com/alakaganaguathoork/local-business/refs/heads/main/terraform/kubernetes/custom-values/grafana/grafana-custom-values-aws.yaml"
       name            = "grafana"
       namespace       = "monitoring"
       repository      = "https://grafana.github.io/helm-charts"
       chart           = "grafana"
     }
     loki = {
-      values_file_url = "https://raw.githubusercontent.com/alakaganaguathoork/local-business/refs/heads/main/helm/helpers/loki/loki-custom-values-aws.yaml"
+      values_file_url = "https://raw.githubusercontent.com/alakaganaguathoork/local-business/refs/heads/main/terraform/kubernetes/custom-values/loki/loki-custom-values-aws.yaml"
       name            = "loki"
       namespace       = "monitoring"
       repository      = "https://grafana.github.io/helm-charts"
       chart           = "loki"
     }
     alloy = {
-      values_file_url = "https://raw.githubusercontent.com/alakaganaguathoork/local-business/refs/heads/main/helm/helpers/alloy/alloy-custom-values-aws.yaml"
+      values_file_url = "https://raw.githubusercontent.com/alakaganaguathoork/local-business/refs/heads/main/terraform/kubernetes/custom-values/alloy/alloy-custom-values-aws.yaml"
       name            = "alloy"
       namespace       = "monitoring"
       repository      = "https://grafana.github.io/helm-charts"
